a6f7a8b9d8175b495d29f49edb70604a6aaaf879
Kali Ruppert <kruppert@smu.edu>
2016-03-24 13:45:51 -0500

    Add welcome method

--- songs.rb
+++ songs.rb
@@ -4,13 +4,26 @@
 
 require "sqlite3"
 
-DB_FILE_NAME = "songs.sqlite3.db"
-SQL_SELECT_GENRES = "SELECT name FROM genres;"
-
-db = SQLite3::Database.new(DB_FILE_NAME)
-
-db.execute(SQL_SELECT_GENRES) do |row|
-	puts row
+def welcome()
+	puts "Welcome to the music database!"
+	puts "   1. Display all song information."
+	puts "   2. Add a new genre."
+	puts "   3. Add a new album."
+	puts "   4. Add a new artist."
+	puts "   5. Add a new song."
+	puts "Enter a choice:"
 end
 
-db.close
+welcome()
+
+
+#DB_FILE_NAME = "songs.sqlite3.db"
+#SQL_SELECT_GENRES = "SELECT name FROM genres;"
+
+#db = SQLite3::Database.new(DB_FILE_NAME)
+
+#db.execute(SQL_SELECT_GENRES) do |row|
+#	puts row
+#end
+
+#db.close
