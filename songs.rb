91745a232bec33decaec95bf61e66b65406d377f
Kali Ruppert <kruppert@smu.edu>
2016-03-25 11:55:16 -0400

    add SQL SELECT constants

--- songs.rb
+++ songs.rb
@@ -7,13 +7,15 @@ require "sqlite3"
 DB_FILE_NAME = "songs.sqlite3.db"
 db = SQLite3::Database.new(DB_FILE_NAME)
 
-SONGS_NAME = "songs.name AS Song"
-GENRES_NAME = "genres.name AS Genre"
-ARTISTS_NAME = "artists.name AS Artist"
-ALBUMS_NAME = "albums.name AS Album"
+#SONGS_NAME = "songs.name AS Song"
+#GENRES_NAME = "genres.name AS Genre"
+#ARTISTS_NAME = "artists.name AS Artist"
+#ALBUMS_NAME = "albums.name AS Album"
 
-SQL_SELECT_SONGS = "SELECT #{SONGS_NAME}, #{GENRES_NAME}, #{ARTISTS_NAME}, #{ALBUMS_NAME} FROM WHERE songs.genre_id = genres.id AND songs.album_id - albums.id AND albums.artist_id = artists.id;"
-
+SQL_SELECT_SONGS = "SELECT songs.name, genres.name, artists.name, albums.name FROM songs, genres, artists, albums WHERE songs.genre_id = genres.id AND songs.album_id - albums.id AND albums.artist_id = artists.id;"
+SQL_SELECT_GENRES = "SELECT name FROM genres;"
+SQL_SELECT_ARTISTS = "SELECT name FROM artists;"
+SQL_SELECT_ALBUMS = "SELECT name FROM albums;"
 
 def welcome()
 	puts "Welcome to the music database!"
@@ -29,7 +31,7 @@ welcome()
 
 
 
-#SQL_SELECT_GENRES = "SELECT name FROM genres;"
+
 
 
 
