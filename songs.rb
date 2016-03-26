7e489d866299e18d8939b16928029fcee5d91466
Kali Ruppert <kruppert@smu.edu>
2016-03-26 17:06:14 -0400

    add add new album functionality

--- songs.rb
+++ songs.rb
@@ -7,18 +7,15 @@ require "sqlite3"
 DB_FILE_NAME = "songs.sqlite3.db"
 db = SQLite3::Database.new(DB_FILE_NAME)
 
-#SONGS_NAME = "songs.name AS Song"
-#GENRES_NAME = "genres.name AS Genre"
-#ARTISTS_NAME = "artists.name AS Artist"
-#ALBUMS_NAME = "albums.name AS Album"
-
 SQL_SELECT_SONGS = "SELECT songs.name, genres.name, artists.name, albums.name FROM songs, genres, artists, albums WHERE songs.genre_id = genres.id AND songs.album_id - albums.id AND albums.artist_id = artists.id;"
-SQL_SELECT_GENRES = "SELECT name FROM genres;"
-SQL_SELECT_ARTISTS = "SELECT name FROM artists;"
-SQL_SELECT_ALBUMS = "SELECT name FROM albums;"
+SQL_SELECT_GENRES = "SELECT * FROM genres;"
+SQL_SELECT_ARTISTS = "SELECT * FROM artists;"
+SQL_SELECT_ALBUMS = "SELECT * FROM albums;"
 
 SQL_INSERT_GENRE = "INSERT INTO genres (name) VALUES (?)"
 SQL_INSERT_ARTIST = "INSERT INTO artists (name) VALUES (?)"
+SQL_INSERT_ALBUM = "INSERT INTO albums (name, artist_id) VALUES (?, ?)"
+SQL_INSERT_SONG = "INSERT INTO songs (name, genre_id, album_id) VALUES (?, ?)"
 
 def welcome()
 	puts "\nWelcome to the music database!"
@@ -39,24 +36,41 @@ when input == "1"
 	db.execute(SQL_SELECT_SONGS) do |song, genre, artist, album|
 		puts "%-25s %-20s %0s %25s" % [song, genre, artist, album]
 	end
+
 when input == "2"
 	puts "\nGenres in database:"
-	db.execute(SQL_SELECT_GENRES) do |genre|
-		puts "    #{genre}"
+	db.execute(SQL_SELECT_GENRES) do |id, genre|
+		puts "\s\s\s" + genre
 	end
 	puts "Enter a new genre name:"
 	new_genre = gets.chomp
 	db.execute(SQL_INSERT_GENRE, [new_genre])
+
 when input == "3"
-	puts "Add new album"
+	puts "\nAlbums in database:"
+	db.execute(SQL_SELECT_ALBUMS) do |id, album, artist|
+		puts "\s\s\s" + album
+	end
+	puts "Enter a new album name:"
+	new_album = gets.chomp
+	puts "\nArtists in database:"
+	puts "\s\s\sID\tArtist Name"
+	db.execute(SQL_SELECT_ARTISTS) do |id, artist|
+		puts "\s\s\s#{id}\t" + artist
+	end
+	puts "Select an artist ID number"
+	artist_id = gets.chomp.to_i
+	db.execute(SQL_INSERT_ALBUM, [new_album, artist_id])
+
 when input == "4"
 	puts "\nArtists in database:"
-	db.execute(SQL_SELECT_ARTISTS) do |artist|
-		puts "    #{artist}"
+	db.execute(SQL_SELECT_ARTISTS) do |id, artist|
+		puts "\s\s\s" + artist
 	end
 	puts "Enter a new artist name:"
 	new_artist = gets.chomp
 	db.execute(SQL_INSERT_ARTIST, [new_artist])
+
 when input == "5"
 	puts "Add new song"
 end
